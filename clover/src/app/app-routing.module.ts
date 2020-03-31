b7b393c4e00df322d4b26045f2543f47271debb0
DavidCeranic <58035567+DavidCeranic@users.noreply.github.com>
2020-03-31 21:12:59 +0200

    #AB5 - Sing-up Component and upgrade Sing-in Component

--- clover/src/app/app-routing.module.ts
+++ clover/src/app/app-routing.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { SingInComponent } from './components/sing-in/sing-in.component';
+import { SingUpComponent } from './components/sing-in/sing-up/sing-up.component';
 
 
 const routes: Routes = [
   {
     path: "sing-in",
     component: SingInComponent
+  },
+  {
+    path: "sing-up",
+    component: SingUpComponent
   }
 ];
 
--- clover/src/app/app.module.ts
+++ clover/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SingInComponent } from './components/sing-in/sing-in.component';
+import { SingUpComponent } from './components/sing-in/sing-up/sing-up.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SingInComponent
+    SingInComponent,
+    SingUpComponent
   ],
   imports: [
     BrowserModule,
