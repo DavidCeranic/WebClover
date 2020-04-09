4df9d2a55cb116aee5ad6cfbbee69d5e4dbc18c9
DavidCeranic <58035567+DavidCeranic@users.noreply.github.com>
2020-04-09 19:28:54 +0200

    #AB10 - Car Component

--- clover/src/app/components/car/car.component.ts
+++ clover/src/app/components/car/car.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-car',
@@ -7,9 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CarComponent implements OnInit {
 
+  SearchCarForm: FormGroup;
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  private initForm() {
+    this.SearchCarForm = new FormGroup({
+      'Place': new FormControl('', Validators.required),
+      'StartDate': new FormControl('', Validators.required),
+      'EndDate': new FormControl('', Validators.required)
+    });
+  }
+
+  onSubmit() {
+    
+  }
 }
