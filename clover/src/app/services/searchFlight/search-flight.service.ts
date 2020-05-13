8b480c5933f9deb2cf3a5f328e244f824d54feb0
Vlada <vlada.toskic@yahoo.com>
2020-05-13 13:10:50 +0200

    Search by flying from

--- clover/src/app/services/searchFlight/search-flight.service.ts
+++ clover/src/app/services/searchFlight/search-flight.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { AbstractFilterParam } from 'src/app/entities/abstract-filter-param/abstract-filter-param';
+import { StringFilterParam } from 'src/app/entities/string-filter-param/string-filter-param';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +8,9 @@ import { Injectable } from '@angular/core';
 export class SearchFlightService {
 
   constructor() { }
+
+
+
+
+  
 }
