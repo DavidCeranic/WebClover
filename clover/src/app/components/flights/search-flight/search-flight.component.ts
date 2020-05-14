463491c685b889ddae84ac981db2fcdc241c517a
DavidCeranic <58035567+DavidCeranic@users.noreply.github.com>
2020-05-14 16:57:26 +0200

    Presek

--- clover/src/app/components/flights/search-flight/search-flight.component.ts
+++ clover/src/app/components/flights/search-flight/search-flight.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit,Input } from '@angular/core';
 })
 export class SearchFlightComponent implements OnInit {
 
-
+  @Input() searchedFlights;
   constructor() { }
 
   ngOnInit(): void {
