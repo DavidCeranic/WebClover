31277d88d6f28084d13de7199894e580797942a2
DavidCeranic <58035567+DavidCeranic@users.noreply.github.com>
2020-08-12 14:14:15 +0200

    #AB28 - Admin Info

---
+++ clover/src/app/components/car/rent-a-car-filter/admin-info/admin-info.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-info',
+  templateUrl: './admin-info.component.html',
+  styleUrls: ['./admin-info.component.css']
+})
+export class AdminInfoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onServiceRate(){
+
+  }
+
+  onCarRate(){
+
+  }
+
+  onGraph(){
+
+  }
+
+  onRevenues(){
+    
+  }
+}
